a7dc7e9bdb559892b38e982cc97565b47686cb5e
Madison Graves <ziaresutton@gmail.com>
2024-11-17 20:15:52 -0500

    create anxiety array of objects

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 console.log("my java is working");
 
+const anxiety = [{_theme:'Anxiety',
+                    _verse: 'Matthew 6:34',
+                    _qoute: 'Therefore do not worry about tomorrow, for tomorrow will worry about itself. Each day has enough trouble of its own.',
+                    _meaning: 'to focus on the present and trust that God will provide for each day, rather than being overwhelmed by future worries.'
+                },
+                {_theme: 'Anxiety',
+                    _verse: '1 Peter 5:7',
+                    _qoute: 'Cast all your anxiety on him because he cares for you.',
+                    _meaning: 'to bring our worries to God, knowing that He cares deeply for us and is ready to take our burdens.'
+                },
+                {_theme:'Anxiety',
+                    _verse:'Isaiah 41:10',
+                    _qoute:'So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand.',
+                    _meaning: 'that we don’t have to fear or be anxious, because God is always with us, ready to strengthen and support us through any situation.'
+                }];
+
+console.log(anxiety);
+
+/*
+const forgiveness = [{},{},{}];
+
+const encouragement = [{},{},{}];
+*/
