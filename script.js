c18932187b7bdd83d9a81641b3538504575f70f1
Madison Graves <ziaresutton@gmail.com>
2024-11-17 23:39:36 -0500

    create forgiveness array of objects

--- script.js
+++ script.js
@@ -18,8 +18,23 @@ const anxiety = [{_theme:'Anxiety',
 
 console.log(anxiety);
 
-/*
-const forgiveness = [{},{},{}];
 
+const forgiveness = [{_theme:'forgiveness',
+                        _verse: 'Colossians 3:13',
+                        _qoute: 'Bear with each other and forgive one another if any of you has a grievance against someone. Forgive as the Lord forgave you.',
+                        _meaning: 'to extend the same grace and forgiveness to others that God has shown to us.'
+                    },
+                    {_theme:'forgiveness',
+                        _verse: 'Matthew 18:21-22',
+                        _qoute: "Then Peter came to Jesus and asked, ‘Lord, how many times shall I forgive my brother or sister who sins against me? Up to seven times?’ Jesus answered, ‘I tell you, not seven times, but seventy-seven times.",
+                        _meaning: "how Jesus teaches that forgiveness should be limitless, just as God's forgiveness for us is without end."
+                    },
+                    {_theme:'forgiveness',
+                        _verse: "Ephesians 4:32",
+                        _qoute: "Be kind and compassionate to one another, forgiving each other, just as in Christ God forgave you.",
+                        _meaning: "that forgiveness should come from a heart of compassion and kindness, reflecting God's forgiveness toward us through Christ."
+                    }];
+/*
 const encouragement = [{},{},{}];
+const messageObj = {};
 */
